b76684ce5a8ed32e530a77cadc35d8d58a774dc0
Giovanni. K <giovannibrandonkornet@gmail.com>
2021-01-03 18:46:26 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,23 @@
-var hamburgerBtn = document.querySelector('.hamburger');
-var openMenu = document.querySelector('.nav-items');
+// Const, omdat variabele niet opnieuw wordt toegewezen aan een ander element. Hij blijft te allen tijden hetzelfde.
 
-hamburgerBtn.addEventListener('click', () => {
-    openMenu.classList.toggle('showMenu');
+const navbar = document.querySelector(".nav-hamburger-mobile")
+const hamburger = document.querySelector(".hamburger")
+const transformHamburger = document.querySelector(".hamburger")
+
+hamburger.addEventListener('click', () => {
+    if (navbar.classList.contains("showNav")) {
+        navbar.classList.toggle("closeNav")
+        navbar.classList.remove("showNav")
+    } else {
+        navbar.classList.toggle("showNav")
+        navbar.classList.remove("closeNav")
+    }
 });
+
+disableScroll = _ => {
+    document.body.classList.toggle('disableScroll');
+}
+
+hamburgerTransform = _ => {
+    transformHamburger.classList.toggle("transformHamburger");
+}
