23674d360b7cf0f6d61e1f262c0e6743510908da
Giovanni. K <giovannibrandonkornet@gmail.com>
2021-01-04 00:52:15 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,36 @@
-// Const, omdat variabele niet opnieuw wordt toegewezen aan een ander element. Hij blijft te allen tijden hetzelfde.
+const navbar = document.querySelector(".nav-hamburger-mobile");
+const hamburger = document.querySelector(".hamburger");
+const transformHamburger = document.querySelector(".hamburger");
+const toggleImages = document.querySelector(
+  ".SVGMarsMobile div:nth-of-type(1)"
+);
+const toggleImages2 = document.querySelector(
+  ".SVGMarsMobile div:nth-of-type(2)"
+);
+const toggledInfo = document.querySelector(".toggled-info");
+const info = document.querySelector(".mars-info");
 
-const navbar = document.querySelector(".nav-hamburger-mobile")
-const hamburger = document.querySelector(".hamburger")
-const transformHamburger = document.querySelector(".hamburger")
-
-hamburger.addEventListener('click', () => {
-    if (navbar.classList.contains("showNav")) {
-        navbar.classList.toggle("closeNav")
-        navbar.classList.remove("showNav")
-    } else {
-        navbar.classList.toggle("showNav")
-        navbar.classList.remove("closeNav")
-    }
+hamburger.addEventListener("click", () => {
+  if (navbar.classList.contains("showNav")) {
+    navbar.classList.toggle("closeNav");
+    navbar.classList.remove("showNav");
+  } else {
+    navbar.classList.toggle("showNav");
+    navbar.classList.remove("closeNav");
+  }
 });
 
-disableScroll = _ => {
-    document.body.classList.toggle('disableScroll');
-}
+disableScroll = (_) => {
+  document.body.classList.toggle("disableScroll");
+};
 
-hamburgerTransform = _ => {
-    transformHamburger.classList.toggle("transformHamburger");
-}
+hamburgerTransform = (_) => {
+  transformHamburger.classList.toggle("transformHamburger");
+};
+
+toggleImage = (_) => {
+  toggleImages.classList.toggle("showImageSVG");
+  toggleImages2.classList.toggle("hideImageSVG");
+  toggledInfo.classList.toggle("hideImageSVG");
+  info.classList.toggle("showImageSVG");
+};
